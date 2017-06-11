096684226381d2eeb5377310c1eb2f78d7e96afa
PHClaus <phhpro@gmail.com>
2017-06-11 14:17:45 +0200

    Delete init_page.php

--- inc/init_page.php
+++
@@ -1,7 +0,0 @@
-<link rel=stylesheet type="text/css" href="style.css"/>
-<div id=ac_page>
-  <p>Welcome to <strong>Atom Chat</strong>.</p>
-  <p>Please enter your user name and password to login and start chatting. New accounts are automatically created.</p>
-  <p>Please note that <em>Login</em> will simply do nothing if either user name or password are missing or invalid.</p>
-  <p id=ac_info><noscript>Java Script disabled or not available!</noscript></p>
-</div>
