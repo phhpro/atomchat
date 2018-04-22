1659cad61e1cfa9163f52893ca3c0ba94f42aeec
PHClaus <15097539+phhpro@users.noreply.github.com>
2018-04-22 12:54:09 +0200

    20180422

---
+++ de.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * Language file English
+ *
+ * @category  PHP_Chat_Scripts
+ * @package   PHP_Atom_Chat
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/atomchat
+ *
+ *
+ * DO NOT CHANGE THE LAYOUT OF THIS FILE !!!
+ */
+
+
+//** Init array
+$lang = array();
+
+
+//** Name and text
+$lang['__name__']    = "Deutsch";
+$lang['__text__']    = "Klicken Sie hier um Deutsch zu wählen";
+
+
+//** Strings
+$lang['apply']       = "Anwenden";
+$lang['apply_title'] = "Klicken Sie hier um die Auswahl anzuwenden";
+
+$lang['by']          = "Angetrieben von";
+$lang['bytes']       = "Bytes";
+
+$lang['characters']  = "Zeichen";
+
+$lang['chat_enter']  = "hat den Chat betreten";
+$lang['chat_leave']  = "hat den Chat verlassen";
+
+$lang['close']       = "Schließen";
+$lang['close_title'] = "Klicken Sie hier um dieses Fenster zu schließen";
+
+$lang['emo']         = "Emoji";
+
+$lang['fail']        = "Ladefehler:";
+$lang['fail_exist']  = "Datei existiert schon!";
+$lang['fail_img']    = "Ungültige Grafik!";
+$lang['fail_size']   = "Datei zu groß!";
+$lang['fail_type']   = "Ungültige oder fehlende Kennung!";
+
+$lang['first']       = "Es hat den Anschein der erste Eintrag gehört Ihnen.";
+
+$lang['get']         = "Klicken Sie hier um eine kostenlose Kopie dieses Skriptes zu erhalten";
+
+$lang['info']        = "Info";
+
+$lang['lang']        = "Sprache";
+$lang['lang_title']  = "Klicken Sie hier um Sprache zu wählen";
+
+$lang['login']       = "Anmelden";
+$lang['login_title'] = "Klicken Sie hier um sich anzumelden";
+
+$lang['name']        = "Name";
+$lang['name_title']  = "Tippen Sie hier um Ihren Namen einzugeben";
+$lang['name_info']   = "Geben Sie den gewünschten Namen ein und drücken auf Anmelden.";
+
+$lang['nolang']      = "Fehlende Sprachdatei!";
+$lang['noscript']    = "Fehlendes JavaScript! Einige Funktionen sind nicht verfügbar.";
+
+$lang['post']        = "Senden";
+$lang['post_title']  = "Klicken Sie hier um Ihre Nachricht zu senden";
+
+$lang['push']        = "Aktualisieren";
+$lang['push_title']  = "Klicken Sie hier um die Sitzung manuell zu aktualisieren";
+
+$lang['quit']        = "Ende";
+$lang['quit_title']  = "Klicken Sie hier um die Sitzung zu beenden";
+
+$lang['save']        = "Sichern";
+$lang['save_title']  = "Klicken Sie hier um die Sitzung zu speichern";
+
+$lang['sel_title']   = "Klicken Sie hier um eine Datei zu wählen";
+
+$lang['set']         = "Einstellungen";
+$lang['set_title']   = "Klicken Sie hier um Einstellungen zu ändern";
+
+$lang['theme']       = "Thema";
+$lang['theme_title'] = "Klicken Sie hier um Thema zu wählen";
+$lang['theme_miss']  = "Fehlende Thema Definition!";
+
+$lang['text']        = "Text";
+$lang['text_title']  = "Tippen Sie hier um Ihre Nachricht einzugeben";
+
+$lang['type_arc']    = "Archiv";
+$lang['type_doc']    = "Dokument";
+$lang['type_img']    = "Grafik";
+$lang['type_snd']    = "Audio";
+$lang['type_vid']    = "Video";
+$lang['type_info']   = "Typeninfo in Einstellungen.";
+$lang['type_list']   = "Erlaubte Dateiarten";
+
+$lang['uc']          = "Benutzer Inhalt";
+$lang['uc_exceed']   = "Datei übersteigt Maximalgröße!";
+$lang['uc_exist']    = "Datei existiert bereits!";
+$lang['uc_fail']     = "Ladefehler:";
+$lang['uc_max']      = "Maximalgröße in Bytes:";
+$lang['uc_noimg']    = "Ungültige Grafik!";
+$lang['uc_nomove']   = "Schreibfehler!";
+$lang['uc_notype']   = "Ungültiger Dateityp!";
+$lang['uc_open']     = "Klicken Sie hier um diese Datei zu öffnen";
---
+++ es.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * Language file Spanish
+ *
+ * @category  PHP_Chat_Scripts
+ * @package   PHP_Atom_Chat
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/atomchat
+ *
+ *
+ * DO NOT CHANGE THE LAYOUT OF THIS FILE !!!
+ */
+
+
+//** Init array
+$lang = array();
+
+
+//** Name and text
+$lang['__name__']    = "Español";
+$lang['__text__']    = "Haga click aquí para seleccionar al Español";
+
+
+//** Strings
+$lang['apply']       = "Aplicar";
+$lang['apply_title'] = "Haga clic aquí para aplicar la seleccion";
+
+$lang['by']          = "Desarrollado por";
+$lang['bytes']       = "bytes";
+
+$lang['characters']  = "caracteres";
+
+$lang['chat_enter']  = "ingresó al chat";
+$lang['chat_leave']  = "dejó el chat";
+
+$lang['close']       = "Cerrar";
+$lang['close_title'] = "Haga clic aquí para cerrar esta ventana";
+
+$lang['emo']         = "Emoji";
+
+$lang['fail']        = "Subida fallida:";
+$lang['fail_exist']  = "¡El archivo ya existe!";
+$lang['fail_img']    = "¡Imagen inválida!";
+$lang['fail_size']   = "¡Archivo demasiado grande!";
+$lang['fail_type']   = "¡Tipo inválido o faltante!";
+
+$lang['first']       = "Parece que el primer mensaje es todo suja.";
+
+$lang['get']         = "Haga clic aquí para obtener una copia gratuita de este script";
+
+$lang['info']        = "Info";
+
+$lang['lang']        = "Idioma";
+$lang['lang_title']  = "Haga clic aquí para seleccionar idioma";
+
+$lang['login']       = "Iniciar";
+$lang['login_title'] = "Haga clic aquí para iniciar sesión";
+
+$lang['name']        = "Nombre";
+$lang['name_title']  = "Escribe aquí para ingresar su nombre";
+$lang['name_info']   = "Ingrese el nombre preferido y presione Iniciar.";
+
+$lang['nolang']      = "¡Falta el archivo de idioma!";
+$lang['noscript']    = "¡Falta JavaScript! Algunas funciones no disponibles.";
+
+$lang['post']        = "Publicar";
+$lang['post_title']  = "Haga clic aquí para publicar su mensaje";
+
+$lang['push']        = "Actualizar";
+$lang['push_title']  = "Haga clic aquí para actualizar manualmente la sesión";
+
+$lang['quit']        = "Salir";
+$lang['quit_title']  = "Haga clic aquí para salir de la sesión";
+
+$lang['save']        = "Guardar";
+$lang['save_title']  = "Haga clic aquí para guardar la sesión";
+
+$lang['sel_title']   = "Haga clic aquí para seleccionar un archivo";
+
+$lang['set']         = "Ajustes";
+$lang['set_title']   = "Haga clic aquí para cambiar la configuración";
+
+$lang['theme']       = "Tema";
+$lang['theme_title'] = "Haga clic aquí para seleccionar tema";
+$lang['theme_miss']  = "¡Falta la definición del tema!";
+
+$lang['text']        = "Texto";
+$lang['text_title']  = "Escribe aquí para ingresar su mensaje";
+
+$lang['type_arc']    = "Archivo";
+$lang['type_doc']    = "Documento";
+$lang['type_img']    = "Imagen";
+$lang['type_snd']    = "Sonido";
+$lang['type_vid']    = "Vídeo";
+$lang['type_info']   = "Info de tipos en Ajustes.";
+$lang['type_list']   = "Tipos Admitidos";
+
+$lang['uc']          = "Contenido De Usuario";
+$lang['uc_exceed']   = "¡El archivo excede el tamaño máximo!";
+$lang['uc_exist']    = "¡El archivo ya existe!";
+$lang['uc_fail']     = "Subida fallida:";
+$lang['uc_max']      = "Tamaño máximo en bytes:";
+$lang['uc_noimg']    = "¡Imagen inválida!";
+$lang['uc_nomove']   = "Error de escritura!";
+$lang['uc_notype']   = "¡Tipo de archivo invalido!";
+$lang['uc_open']     = "Haga clic aquí para abrir este archivo";
