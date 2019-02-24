8da8b52f3f4e7a60f054cc365cbc10267390e753
ErrorNow <david.schouten@live.nl>
2019-02-24 18:32:53 +0100

    Creat nl.php
    Added Dutch language support

---
+++ lang/nl.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * Language file Dutch
+ *
+ * @category  PHP_Chat
+ * @package   PHP_Atomchat
+ * @author    D Schouten <david@schouten.io>
+ * @copyright 2015 - 2019 D Schouten
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/atomchat
+ */
+//** Init array
+$lang = array();
+//** Name and text
+$lang['__name__']    = "Nederlands";
+$lang['__text__']    = "Klik hier om Nederlands te selecteren";
+//** Strings
+$lang['apply_title'] = "Klik hier om de selectie toe te passen";
+$lang['close_title'] = "Klik hier om het scherm af te sluiten";
+$lang['conf']        = "Instellingen";
+$lang['conf_title']  = "Klik hier om instellingen aan te passen";
+$lang['emo']         = "Emoji";
+$lang['first']       = "Het eerste bericht is voor jou.";
+$lang['get']         = "Klik hier om een gratis kopie van dit script te krijgen";
+$lang['info']        = "Info";
+$lang['lang']        = "Taal";
+$lang['lang_title']  = "Klik hier om een taal te kiezen";
+$lang['lang_miss']   = "Taal bestand niet gevonden!";
+$lang['log_reset']   = "Reset";
+$lang['login_title'] = "Klik hier om in te loggen";
+$lang['name']        = "Naam";
+$lang['name_title']  = "Type hier je naam";
+$lang['name_info']   = "Voer je naam in en klik op de knop.";
+$lang['noscript']    = "Javascript functies niet beschikbaar!";
+$lang['post_title']  = "Klik hier om je bericht te plaatsen!";
+$lang['quit_title']  = "Klik hier om de sessie te eindigen";
+$lang['save_title']  = "Klik hier om de sessie te bewaren";
+$lang['theme']       = "Thema";
+$lang['theme_title'] = "Klik hier om een thema te kiezen";
+$lang['theme_miss']  = "Geen thema beschrijving gevonden!";
+$lang['text']        = "Tekst";
+$lang['text_title']  = "Type hier je bericht";
+$lang['up']          = "Uploads";
+$lang['up_arc']      = "Archief";
+$lang['up_b64']      = "Base64";
+$lang['up_doc']      = "Document";
+$lang['up_img']      = "Afbeelding";
+$lang['up_max']      = "Maximum grootte";
+$lang['up_open']     = "Klik hier om dit bestand te openen";
+$lang['up_select']   = "Klik hier om een bestand te selecteren";
+$lang['up_snd']      = "Audio";
+$lang['up_vid']      = "Video";
